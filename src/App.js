ceb269ee14fb68aa3d144202d2e36ff5db01d58a
TajwrSaiyeed <rjabid36@gmail.com>
2022-09-29 20:53:53 +0600

    full and final update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast} from 'react-toastify';
 import './App.css';
 import Courses from './components/Courses/Courses';
 import Info from './components/Info/Info'
@@ -8,6 +10,7 @@ function App() {
   const exerciseTime = (selectCourse) => {
     const newCourse = [...course, selectCourse];
     setCourse(newCourse);
+    toast.success("Successfully Added",{position: toast.POSITION.TOP_CENTER});
   }
   return (
     <div className="App">
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import "./Button.css"
 const Button = (props) => {
     const {course,text,style,exerciseTime} = props;
     return (
-        <button onClick={() => exerciseTime(course)} className='cart-btn' style={style}>{text}</button>
+        <button onClick={() => {exerciseTime(course)}} className='cart-btn' style={style}>{text}</button>
     );
 };
 
